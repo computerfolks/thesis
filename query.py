09e6ae82e9f6cf58da1e3f985ddecaba4124b3eb
Jacob Windows <jacobweissman18@gmail.com>
2023-11-26 09:39:29 -0500

    query tested and commented

--- query.py
+++ query.py
@@ -1,18 +1,25 @@
 import urllib.request
 import json
 
-
+# visual crossing api link
 base_url = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/'
 
+# get api key which is not uploaded because it is in a .gitignore
 with open('api_key.txt', 'r') as file:
   api_key = file.read().strip()
 
 def query_builder(start_date, end_date, zip_code, units = 'us'):
     """
-    zip_code: valid US zip
-    start_date: first date in the range of dates to collect data
-    end_date: end date in the range of dates to collect data
-    units: which system of units to use, options include ['us', 'uk', 'metric', 'base']
+    construct query for api call, return string containing the api call link
+
+    input:
+      zip_code: valid US zip
+      start_date: first date in the range of dates to collect data
+      end_date: end date in the range of dates to collect data
+      units: which system of units to use, options include ['us', 'uk', 'metric', 'base']
+
+    output:
+      url: api url
 
     possible future functionality:
     include: list of sections to include in query response, 'days' by default to exclude hourly data
@@ -25,7 +32,7 @@ def query_builder(start_date, end_date, zip_code, units = 'us'):
 
 def query_attempt(url):
     """
-    function which attempts to query for the weather data and provides a result
+    function which attempts to make an api call to the 'url' for the weather data and returns the raw result
     """
     print(f"Running query URL: {url}")
     
@@ -39,14 +46,16 @@ def query_attempt(url):
 
 def get_query_result_for_date_range_zip_code(start_date, end_date, zip_code):
   """
-  get query results for a given start_date, end_date, zip_code
+  get complete query results for a given start_date, end_date, zip_code
 
   input:
     start_date, end_date: dates in correct format, representing the range to query
     zip_code: valid US zip code
 
   output:
-    tuple containing as first value the input, as second value the raw_result from query
+    a tuple with two values
+    the first value is a tuple of the input to the function
+    the second value is the raw_result from the api call
 
   """
 
@@ -61,7 +70,9 @@ def get_query_result_for_date_range_zip_code(start_date, end_date, zip_code):
 
 def get_query_results_for_date_range_zip_codes_dict(date_range_zip_codes_dict):
   """
-  given dictionary containing as keys tuples of date ranges and as values the list of zip codes for the date range, 
+  given dictionary containing:
+    as keys: tuples of date ranges
+    as values: the list of zip codes for the date range
   query and return query result for each date range - zip code pair, and return with format of dictionary with key : value as
   start_date, end_date, zip_code : query_result
 
@@ -93,9 +104,5 @@ def get_query_results_for_date_range_zip_codes_dict(date_range_zip_codes_dict):
 
 # testing
 if __name__ == '__main__':
-    # query_current = query_builder('11559', '2023-10-05', '2023-10-07')
-    # raw_answer = query_attempt(query_current)
-    # print(raw_answer)
-    # test_dict = {('2012-01-01', '2012-01-03'): ['11516', '11559', '11598']}
     test_dict = {('2022-12-30', '2023-1-5'): ['11559', '33433'], ('2021-12-30', '2022-01-05'): ['11559', '33433'], ('2023-6-10', '2023-06-16'): ['11559']}
     print(get_query_results_for_date_range_zip_codes_dict(test_dict))
