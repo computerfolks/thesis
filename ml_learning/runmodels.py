aa9234e8f653a69656763dff4e6dbf78ed564d67
Jacob Windows <jacobweissman18@gmail.com>
2023-11-26 12:07:59 -0500

    tested runmodels

--- ml_learning/runmodels.py
+++ ml_learning/runmodels.py
@@ -10,7 +10,7 @@ from sklearn.ensemble import RandomForestRegressor
 
 target = 'number_of_rides'
 
-def test_all_models(predictors, predictors_description, train_val_csv, train_csv, test_csv):
+def test_all_models_on_validation(predictors, predictors_description, train_val_csv, train_csv, val_csv):
     """
     this function is used to run all models with all lists of predictors on validation data using training data
     the best performing model (and only that model) was then tested on test data and reported
@@ -30,41 +30,41 @@ def test_all_models(predictors, predictors_description, train_val_csv, train_csv
 
     # multilayer perceptron
     hyperparams = find_mlp_optimal_hyperparams(train_val_csv, target, predictors)
-    mlp_score = mlp_predict(train_csv, test_csv, target, predictors, hyperparams)
+    mlp_score = mlp_predict(train_csv, val_csv, target, predictors, hyperparams)
     scores[f'{predictors_description} MultiLayer Perceptron'] = mlp_score
 
 
 
     # regression
     eln = ElasticNet(random_state = random_seed, max_iter = 10000)
-    eln_score = lr_predict(train_val_csv, train_csv, test_csv, target, eln, predictors)
+    eln_score = lr_predict(train_val_csv, train_csv, val_csv, target, eln, predictors)
     scores[f'{predictors_description} Elastic Net Regression'] = eln_score
 
     lss = Lasso(random_state = random_seed, max_iter = 10000)
-    lss_score = lr_predict(train_val_csv, train_csv, test_csv, target, lss, predictors)
+    lss_score = lr_predict(train_val_csv, train_csv, val_csv, target, lss, predictors)
     scores[f'{predictors_description} LASSO Regression'] = lss_score
     
     rdg = Ridge(random_state = random_seed, max_iter = 10000)
-    rdg_score = lr_predict(train_val_csv, train_csv, test_csv, target, rdg, predictors)
+    rdg_score = lr_predict(train_val_csv, train_csv, val_csv, target, rdg, predictors)
     scores[f'{predictors_description} Ridge Regression'] = rdg_score
 
     hbr = HuberRegressor(max_iter = 10000)
-    hbr_score = lr_predict(train_val_csv, train_csv, test_csv, target, hbr, predictors)
+    hbr_score = lr_predict(train_val_csv, train_csv, val_csv, target, hbr, predictors)
     scores[f'{predictors_description} Huber Regression'] = hbr_score
 
     clr = LinearRegression()
-    clr_score = predict(train_csv, test_csv, target, clr, predictors)
+    clr_score = predict(train_csv, val_csv, target, clr, predictors)
     scores[f'{predictors_description} Classical Linear Regression'] = clr_score
 
     svm = LinearSVR(random_state = random_seed, max_iter = 100000)
-    svm_score = predict(train_csv, test_csv, target, svm, predictors)
+    svm_score = predict(train_csv, val_csv, target, svm, predictors)
     scores[f'{predictors_description} Linear Support Vector'] = svm_score
 
 
 
     # random forest
     rf = RandomForestRegressor(random_state=random_seed)
-    rf_score = predict(train_csv, test_csv, target, rf, predictors)
+    rf_score = predict(train_csv, val_csv, target, rf, predictors)
     scores[f'{predictors_description} Random Forest'] = rf_score
 
     return scores
@@ -80,24 +80,23 @@ if __name__ == '__main__':
     train_val_csv = 'ml_learning/n_train_val.csv'
     train_csv = 'ml_learning/n_train.csv'
     val_csv = 'ml_learning/n_val.csv'
-    test_csv = 'ml_learning/n_test.csv'
 
-    baseline_result = test_all_models(baseline_predictors, 'Baseline', train_val_csv, train_csv, val_csv)
+    baseline_result = test_all_models_on_validation(baseline_predictors, 'Baseline', train_val_csv, train_csv, val_csv)
     print(baseline_result)
     all_results.update(baseline_result)
     print(all_results)
 
-    selection_result = test_all_models(selection_predictors, 'Feature Selection', fs_train_val_csv, fs_train_csv, fs_val_csv)
+    selection_result = test_all_models_on_validation(selection_predictors, 'Feature Selection', fs_train_val_csv, fs_train_csv, fs_val_csv)
     print(selection_result)
     all_results.update(selection_result)
     print(all_results)
 
-    all_features_result = test_all_models(all_predictors, 'All Features', train_val_csv, train_csv, val_csv)
+    all_features_result = test_all_models_on_validation(all_predictors, 'All Features', train_val_csv, train_csv, val_csv)
     print(all_features_result)
     all_results.update(all_features_result)
     print(all_results)
 
-    domain_result = test_all_models(domain_predictors, 'Domain Expert', train_val_csv, train_csv, val_csv)
+    domain_result = test_all_models_on_validation(domain_predictors, 'Domain Expert', train_val_csv, train_csv, val_csv)
     print(domain_result)
     all_results.update(domain_result)
     print(all_results)
